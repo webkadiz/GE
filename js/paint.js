a6c452de6068e1f007440e132c558703d818462e
vlad <vlados200257@gmail.com>
2018-03-20 06:52:48 +0300

    second continue

--- js/paint.js
+++ js/paint.js
@@ -1,111 +1,49 @@
 var canvas = document.querySelector("#canvas");
 var canvas_wrapper = document.querySelector(".canvas-wrapper");
 var c = canvas.getContext("2d");
-
+var counter = 0;
 const widthHeader = 150;
 
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight - 150;
 
-// pencil
-
-// function moveToPencil(e) {
-//   let arr_canvas = document.querySelectorAll(".canvas");
-//   let x = e.clientX;
-//   let y = e.clientY - widthHeader;
-
-//   c.beginPath();
-//   c.moveTo(x, y);
-
-//   canvas.onmousemove = function(e) {
-//     let x = e.clientX;
-//     let y = e.clientY - widthHeader;
-//     c.lineTo(x, y);
-//     c.stroke();
-//   };
-//   canvas.onmouseup = function() {
-//     canvas.onmousemove = null;
-//     for (let i = 0; i < arr_canvas.length; i++) {
-//       arr_canvas[i].onmousemove = null;
-//     }
-//   };
-//   for (let i = 0; i < arr_canvas.length; i++) {
-//     arr_canvas[i].onmouseover = function() {
-//       arr_canvas[i].onmousemove = function(e) {
-//         let x = e.clientX;
-//         let y = e.clientY - widthHeader;
-//         c.lineTo(x, y);
-//         c.stroke();
-//       };
-//       arr_canvas[i].onmouseout = function() {
-//         arr_canvas[i].onmousemove = null;
-//       };
-//       arr_canvas[i].onmouseup = function() {
-//         arr_canvas[i].onmousemove = null;
-//         canvas.onmousemove = null;
-//       };
-//     };
-//   }
-// }
-
 function moveToPencilWrapper(e) {
   let target = e.target;
-  let x1 = e.clientX;
-  let y1 = e.clientY - widthHeader;
+  let x = e.clientX - (parseInt(target.style.left) || 0);
+  let y = e.clientY - widthHeader - (parseInt(target.style.top) || 0);
+
+  c = target.getContext("2d");
 
   c.beginPath();
-  c.moveTo(x1, y1);
+  c.moveTo(x, y);
 
-  target.onmousemove = function(e) {
-    let x = e.clientX;
-    let y = e.clientY - widthHeader;
+  canvas_wrapper.onmousemove = function(e) {
+    let current_target = e.target;
+    let x = e.clientX - (parseInt(current_target.style.left) || 0);
+    let y = e.clientY - widthHeader - (parseInt(current_target.style.top) || 0);
+
+    c = current_target.getContext("2d");
+    console.log(current_target !== target);
+    if (current_target !== target) {
+      target = current_target;
+      c.beginPath();
+      c.moveTo(x, y);
+    }
+
     c.lineTo(x, y);
     c.stroke();
   };
-
-  target.onmouseout = function(e) {
-    let relatedTarget = e.relatedTarget;
-    console.log(relatedTarget);
-    if (relatedTarget !== null) {
-      relatedTarget.onmousemove = function(e) {
-        console.log(1);
-        let x = e.clientX;
-        let y = e.clientY - widthHeader;
-        c.lineTo(x, y);
-        c.stroke();
-      };
-      relatedTarget.onmouseup = function() {
-        target.onmousemove = null;
-        target.onmouseout = null;
-        target.onmouseover = null;
-        relatedTarget.onmousemove = null;
-      };
-    }
-  };
-  target.onmouseover = function(e) {
-    let relatedTarget = e.relatedTarget;
-    if (relatedTarget !== null) {
-      console.log(relatedTarget);
-      relatedTarget.onmousemove = null;
-    }
-  };
-  target.onmouseup = function() {
-    target.onmousemove = null;
-    target.onmouseout = null;
-    target.onmouseover = null;
+  canvas_wrapper.onmouseup = function() {
+    canvas_wrapper.onmousemove = null;
   };
 }
 
-document.querySelector(".pencil").addEventListener("click", function(e) {
+document.querySelector(".pencil-badge").addEventListener("click", function(e) {
   e.preventDefault();
   this.classList.toggle("active");
 
   if (this.classList.contains("active")) {
     canvas_wrapper.addEventListener("mousedown", moveToPencilWrapper);
-    // canvas.addEventListener("mousedown", moveToPencil);
-    // for (let i = 0; i < arr_canvas.length; i++) {
-    //   arr_canvas[i].addEventListener("mousedown", moveToPencil);
-    // }
   } else {
     canvas_wrapper.removeEventListener("mousedown", moveToPencilWrapper);
   }
@@ -119,34 +57,22 @@ function moveToRubber(e) {
   let x = e.clientX;
   let y = e.clientY - widthHeader;
 
-  c.clearRect(x, y, 16, 16);
-
   canvas_wrapper.onmousemove = function(e) {
-    let x = e.clientX;
-    let y = e.clientY - widthHeader;
+    console.log(e);
+    let target = e.target;
+    c = target.getContext("2d");
+    let x = e.clientX - (parseInt(target.style.left) || 0);
+    let y = e.clientY - widthHeader - (parseInt(target.style.top) || 0);
+    console.log(x);
+    console.log(y);
     c.clearRect(x, y, 16, 16);
-  };
-  canvas_wrapper.onmouseout = function(e) {
-    let relatedTarget = e.relatedTarget;
-    console.log(relatedTarget);
-    if (relatedTarget !== null) {
-      let new_c = document.querySelector(".canvas").getContext("2d");
-      relatedTarget.onmousemove = function(e) {
-        let x = e.clientX;
-        console.log(new_c);
-
-        let y = e.clientY - widthHeader;
-        console.log(y);
-        new_c.clearRect(x, y, 16, 16);
-      };
-    }
   };
   canvas_wrapper.onmouseup = function() {
     canvas_wrapper.onmousemove = null;
   };
 }
 
-document.querySelector(".rubber").addEventListener("click", function(e) {
+document.querySelector(".rubber-badge").addEventListener("click", function(e) {
   e.preventDefault();
   canvas_wrapper.classList.toggle("rubber-canvas-active");
   this.classList.toggle("active");
@@ -159,6 +85,93 @@ document.querySelector(".rubber").addEventListener("click", function(e) {
 });
 
 //rubber
+
+//text
+
+function create_textarea(x, y) {
+  var textarea = document.createElement("textarea");
+
+  textarea.style.left = x + "px";
+  textarea.style.top = y + "px";
+
+  textarea.classList.add("canvas-textarea-text");
+
+  canvas_wrapper.appendChild(textarea);
+}
+
+counter = 0;
+function text_click(e) {
+  let x = e.clientX;
+  let y = e.clientY - widthHeader;
+  let x1 = 0;
+  let y1 = 0;
+  if (counter == 0) {
+    create_textarea(x, y);
+    counter++;
+  }
+
+  textarea = document.querySelector(".canvas-textarea-text");
+  if (textarea) {
+    textarea.onblur = function() {
+      let text = textarea.value;
+      let new_canvas = document.createElement("canvas");
+
+      let x = 0;
+      let y = 30;
+
+      new_canvas.classList.add("canvas");
+      new_canvas.classList.add("canvas-text");
+      new_canvas.style.left = textarea.style.left;
+      new_canvas.style.top = textarea.style.top;
+
+      canvas_wrapper.appendChild(new_canvas);
+      c = new_canvas.getContext("2d");
+
+      text = text.split("\n");
+      let width = c.measureText(text[0]).width;
+      for (let i = 0; i < text.length; i++) {
+        if (width < c.measureText(text[i]).width)
+          width = c.measureText(text[i]).width;
+      }
+      new_canvas.width = width * 2.2;
+      new_canvas.height = text.length * 30;
+      console.log(width);
+      for (let i = 0; i < text.length; i++) {
+        c.font = "400 22px sans-serif";
+        c.fillText(text[i], x, y);
+        y += 30;
+      }
+
+      textarea_remember = canvas_wrapper.removeChild(textarea);
+      console.log(textarea_remember.value);
+      document
+        .querySelector(".canvas-text")
+        .addEventListener("dblclick", function() {
+          console.log(234);
+          canvas_wrapper.removeChild(this);
+          create_textarea(
+            parseInt(new_canvas.style.left),
+            parseInt(new_canvas.style.top)
+          );
+          console.log(textarea_remember.value);
+          document.querySelector(".canvas-textarea-text").value =
+            textarea_remember.value;
+        });
+    };
+  }
+}
+
+document.querySelector(".text-badge").addEventListener("click", function(e) {
+  e.preventDefault();
+  this.classList.toggle("active");
+  if (this.classList.contains("active")) {
+    canvas_wrapper.addEventListener("mousedown", text_click);
+  } else {
+    canvas_wrapper.removeEventListener("mousedown", text_click);
+  }
+});
+
+//text
 
 //shapes
 
