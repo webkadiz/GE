f811434051c26f11517b985cd2a486598c8166b0
vlad <vlados200257@gmail.com>
2018-03-28 00:24:16 +0300

    day 10

--- js/paint.js
+++ js/paint.js
@@ -1,17 +1,50 @@
-var canvas = document.querySelector("#canvas");
-var canvas_wrapper = document.querySelector(".canvas-wrapper");
-var c = canvas.getContext("2d");
-var counter = 0;
-let prev_target;
-const widthHeader = 150;
+let canvas = document.querySelector("#canvas"); //главный канвас
+let canvas_wrapper = document.querySelector(".canvas-wrapper"); //обертка канвасов
+let c = canvas.getContext("2d"); //контекст главного канваса
+let counter = 0; // просто счетчик если понадобится
+let prev_target; // предыдущая нажатая иконка , в обработчике на .main-nav - шапка painta
+let temp_target; // предыдущий канвас , используется в функции move_canvas
+let new_canvas;
+let new_c;
+let script = document.querySelector("script"); // последний script на странице
+const widthHeader = 171; // константная ширина шапки
+let zoom = 1;
 
-let clear_coords = [];
-let arr_delete_fill_color = [];
+let clear_coords = []; // координаты пройденные с помощью ластика
 let arr_textarea = [];
-let path = [];
-let all_path = [];
-let path_delete = [];
-let arr_context = [];
+let path = []; // все пути которые можно рисовать
+let all_path = []; // обсолютно все
+let path_delete = []; // удаленные пути через ctrl-z
+let arr_context = []; // все контексты которые создаются
+
+//все значки с функциями для делегирования из .nav-options
+let arr_settings = [
+  {
+    class_badge: "options-window-size",
+    class_settings: "settings-window-size",
+    value_input: document.querySelectorAll(".size-wrapper input"),
+    appear_func: function() {
+      let elem = document.querySelector("." + this.class_settings);
+
+      this.value_input[0].value = canvas.width;
+      this.value_input[1].value = canvas.height;
+
+      elem.style.display = "block";
+      elem.classList.remove("zoomOut");
+      elem.classList.add("zoomIn");
+
+      centering_element(elem);
+    },
+    service_func: function() {
+      canvas_wrapper.style.width = parseInt(this.value_input[0].value) + "px";
+      canvas.width = parseInt(this.value_input[0].value);
+
+      canvas_wrapper.style.height = parseInt(this.value_input[1].value) + "px";
+      canvas.height = parseInt(this.value_input[1].value);
+    }
+  }
+];
+// все значки с функциями для делегирования из tools
 let arr_class = [
   {
     class: "pencil-badge",
@@ -22,11 +55,15 @@ let arr_class = [
     class: "rubber-badge",
     func: moveToRubber,
     event: "mousedown",
-    add_func: function(string) {
-      if (string == "remove")
+    string: "add",
+    service_func: function(string) {
+      if (string == "remove") {
         canvas_wrapper.classList.remove("rubber-canvas-active");
-      else if (string == "add")
+        this.string = "add";
+      } else if (string == "add") {
         canvas_wrapper.classList.add("rubber-canvas-active");
+        this.string = "remove";
+      }
     }
   },
   {
@@ -43,69 +80,130 @@ let arr_class = [
     class: "square-badge",
     func: beginRect,
     event: "mousedown"
+  },
+  {
+    class: "line-badge",
+    func: draw_line,
+    event: "mousedown"
+  },
+  {
+    class: "move-badge",
+    func: move_canvas,
+    event: "mousedown",
+    service_func: function() {
+      if (temp_target) {
+        temp_target.style.border = "";
+      }
+    }
   }
 ];
 
-canvas.width = document.documentElement.clientWidth;
-canvas.height = document.documentElement.clientHeight - 150;
+// устанавливаем ширину , высоту главному canvas
+canvas_wrapper.style.zoom = 1;
+canvas_wrapper.style.width = document.documentElement.clientWidth + "px";
+canvas_wrapper.style.height =
+  document.documentElement.clientHeight - widthHeader + "px";
 
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight - widthHeader;
+
+document.querySelector(".apply").addEventListener("click", function(e) {
+  let target = this.parentElement;
+  target.classList.remove("zoomIn");
+  target.classList.add("zoomOut");
+
+  for (let i = 0; i < arr_settings.length; i++) {
+    if (target.classList.contains(arr_settings[i].class_settings)) {
+      arr_settings[i].service_func();
+    }
+  }
+
+  setTimeout(() => {
+    target.style.display = "";
+  }, 500);
+});
+
+document.querySelector(".options").addEventListener("click", function(e) {
+  e.preventDefault();
+  let target = e.target;
+
+  for (let i = 0; i < arr_settings.length; i++) {
+    if (e.target.classList.contains(arr_settings[i].class_badge)) {
+      arr_settings[i].appear_func();
+    }
+  }
+});
+
+// используем делегирование на шапке painta
 document.querySelector(".main-nav").onclick = function(e) {
   let target = e.target;
+  let new_target;
 
   e.preventDefault();
   counter = 0;
 
+  function check() {
+    if (prev_target) {
+      canvas_wrapper.removeEventListener(
+        prev_target.arr_class.event,
+        prev_target.arr_class.func
+      );
+      if (prev_target.arr_class.service_func) {
+        prev_target.arr_class.service_func(prev_target.arr_class.string);
+      }
+      prev_target.target.classList.remove("active");
+    }
+  }
+
   for (let i = 0; i < arr_class.length; i++) {
-    let new_target = target.closest("." + arr_class[i].class);
+    new_target = target.closest("." + arr_class[i].class);
 
     if (new_target) {
       counter++;
-      if (prev_target) {
-        canvas_wrapper.removeEventListener(
-          prev_target.arr.event,
-          prev_target.arr.func
-        );
-        if (prev_target.arr.add_func) {
-          prev_target.arr.add_func("remove");
-        }
-        prev_target.target.classList.remove("active");
-      }
+
+      check();
 
       canvas_wrapper.addEventListener(arr_class[i].event, arr_class[i].func);
 
-      if (arr_class[i].add_func) {
-        arr_class[i].add_func("add");
+      if (arr_class[i].service_func) {
+        arr_class[i].service_func(arr_class[i].string);
       }
 
       prev_target = {
         target: document.querySelector("." + arr_class[i].class),
-        arr: arr_class[i]
+        arr_class: arr_class[i]
       };
 
       new_target.classList.add("active");
     }
   }
+
   if (counter == 0) {
-    if (prev_target) {
-      console.log(1);
-      canvas_wrapper.removeEventListener(
-        prev_target.arr.event,
-        prev_target.arr.func
-      );
-      prev_target.target.classList.remove("active");
-    }
+    check();
+    prev_target = null;
   }
 };
 
-document.addEventListener("keyup", function(e) {
+document.addEventListener("keydown", function(e) {
   if ((e.keyCode == 90 && e.ctrlKey) || (e.keyCode == 90 && e.metaKey)) {
     path_remove();
   }
+
+  // увеличение масштаба
+  if (e.keyCode == 187 && e.ctrlKey && e.altKey) {
+    canvas_wrapper.style.zoom = parseFloat(canvas_wrapper.style.zoom) + 0.05;
+    zoom -= 0.055;
+  }
+  // уменьшение масштаба
+  if (e.keyCode == 189 && e.ctrlKey && e.altKey) {
+    canvas_wrapper.style.zoom = parseFloat(canvas_wrapper.style.zoom) - 0.05;
+    zoom += 0.055;
+  }
 });
 
+// удаление пути через ctrl-z
 function path_remove() {
   arr_context.forEach(item => {
-    console.log(item);
     item.width = item.width;
     item.height = item.height;
   });
@@ -116,14 +214,24 @@ function path_remove() {
   let all_path_last = all_path[all_path.length - 1];
   let path_last = path[path.length - 1];
 
+  // возвращаем canvas на прежнее место после передвижения
+  if (all_path_last && all_path_last.move_canvas) {
+    all_path_last.target.style.left = all_path_last.left;
+    all_path_last.target.style.top = all_path_last.top;
+  }
+
+  // удаляем путь ластика если он есть
   if (all_path_last && all_path_last.clear) {
     clear_coords.pop();
   }
 
+  // удаляем цвет если есть
   if (all_path_last && all_path_last.fill_pop) {
     all_path_last.target.fillStyle.pop();
   }
 
+  //если это путь то выкидываем еще и из массива путей , которые можно рисовать
+  //и проверяем надо ли удалить елемент из DOM-дерева
   if (all_path_last && all_path_last.isPath) {
     if (path_last && path_last.remove) {
       path_last.remove.remove();
@@ -132,8 +240,10 @@ function path_remove() {
     path.pop();
   }
 
+  // удаем последнее действие - сама реализация ctrl-z
   path_delete.push(all_path.pop());
 
+  // рисуем все заново без одного элемента
   path.forEach(item => {
     if (item.fill) {
       item.context.fillStyle = item.fillStyle[item.fillStyle.length - 1];
@@ -153,9 +263,7 @@ function path_remove() {
     item.context.stroke(item.path);
   });
 
-  console.log(all_path);
-  console.log(clear_coords);
-
+  // закрашиваем заново все ластиком
   clear_coords.forEach(item => {
     item.coords.forEach(item => {
       c.fillStyle = "#fff";
@@ -164,10 +272,63 @@ function path_remove() {
   });
 }
 
+// передвижение canvas
+function move_canvas(e) {
+  let target = e.target;
+  if (e.target == canvas) {
+    target = canvas_wrapper;
+  }
+  if (temp_target == target) {
+    let x = e.clientX;
+    let y = e.clientY;
+    if (temp_target == canvas) {
+      temp_target = canvas_wrapper;
+    }
+    let begin_x = parseInt(temp_target.style.left) || 0;
+    let begin_y = parseInt(temp_target.style.top) || 0;
+
+    // запоминаем передвижения для отмены через ctrl-z
+
+    all_path.push({
+      isPath: false,
+      left: temp_target.style.left,
+      top: temp_target.style.top,
+      move_canvas: true,
+      target: temp_target
+    });
+
+    canvas_wrapper.onmousemove = function(e) {
+      let new_x = e.clientX - x;
+      let new_y = e.clientY - y;
+      //console.log(temp_target == this);
+      temp_target.style.left = begin_x + new_x + "px";
+      temp_target.style.top = begin_y + new_y + "px";
+
+      if (parseInt(temp_target.style.top) < 0) {
+        temp_target.style.top = "0px";
+      }
+    };
+
+    temp_target.onmouseup = function() {
+      canvas_wrapper.onmousemove = null;
+    };
+
+    document.onmouseup = function() {
+      canvas_wrapper.onmousemove = null;
+    };
+  } else if (temp_target !== undefined) {
+    temp_target.style.border = "";
+  }
+
+  temp_target = target;
+  target.style.border = "1px dashed black";
+}
+
+// рисование карандашом
 function draw_pencil(e) {
   let target = e.target;
-  let x = e.clientX - (parseInt(target.style.left) || 0);
-  let y = e.clientY - widthHeader - (parseInt(target.style.top) || 0);
+  let x = e.clientX * zoom - (parseInt(target.style.left) || 0);
+  let y = e.clientY * zoom - widthHeader - (parseInt(target.style.top) || 0);
 
   c = target.getContext("2d");
 
@@ -191,8 +352,14 @@ function draw_pencil(e) {
 
   canvas_wrapper.onmousemove = function(e) {
     let current_target = e.target;
-    let x = e.clientX - (parseInt(current_target.style.left) || 0);
-    let y = e.clientY - widthHeader - (parseInt(current_target.style.top) || 0);
+    let x = e.clientX * zoom - (parseInt(current_target.style.left) || 0);
+    let y =
+      e.clientY * zoom -
+      widthHeader -
+      (parseInt(current_target.style.top) || 0);
+
+    console.log(e.clientX);
+    console.log(e.clientX * zoom);
 
     c = current_target.getContext("2d");
 
@@ -227,10 +394,7 @@ function draw_pencil(e) {
   };
 }
 
-// pencil
-
-//rubber
-
+// ластик
 function moveToRubber(e) {
   let x = e.clientX;
   let y = e.clientY - widthHeader;
@@ -270,9 +434,7 @@ function moveToRubber(e) {
   };
 }
 
-//rubber
-
-//text
+//рисование текста
 
 function draw_text(text, c, line_height, font_size, font) {
   let temp = line_height;
@@ -284,6 +446,7 @@ function draw_text(text, c, line_height, font_size, font) {
   line_height = temp;
 }
 
+// создания поля ввода для текста
 function create_textarea(x, y) {
   var textarea = document.createElement("textarea");
 
@@ -298,6 +461,7 @@ function create_textarea(x, y) {
   return textarea;
 }
 
+//вставка поля ввода или canvasa в документ
 function text_click(e) {
   let x = e.clientX;
   let y = e.clientY - widthHeader;
@@ -386,9 +550,7 @@ function text_click(e) {
   }
 }
 
-//text
-
-//pouring
+//заливка
 
 function fill(e) {
   let x = e.clientX - (parseInt(e.target.style.left) || 0);
@@ -456,8 +618,6 @@ function fill(e) {
   }
 }
 
-//pouring
-
 //shapes
 
 document
@@ -468,6 +628,24 @@ document
       .classList.toggle("dropdown-shapes-wrapper-active");
   });
 
+//создание нового canvas
+function create_canvas(x1, y1, ...value_class) {
+  new_canvas = document.createElement("canvas");
+  new_c = new_canvas.getContext("2d");
+  new_canvas.classList.add("canvas");
+
+  value_class.forEach(item => {
+    new_canvas.classList.add(item);
+  });
+
+  new_canvas.style.left = x1 + "px";
+  new_canvas.style.top = y1 + "px";
+  new_canvas.width = 0;
+  new_canvas.height = 0;
+}
+
+// рисование прямоугольника
+
 function beginRect(e) {
   let x1 = e.clientX;
   let y1 = e.clientY - widthHeader;
@@ -475,14 +653,7 @@ function beginRect(e) {
   let x = 0;
   let current_path = {};
 
-  var new_canvas = document.createElement("canvas");
-  let new_c = new_canvas.getContext("2d");
-  new_canvas.classList.add("canvas");
-
-  new_canvas.style.left = x1 + "px";
-  new_canvas.style.top = y1 + "px";
-  new_canvas.width = 0;
-  new_canvas.height = 0;
+  create_canvas(x1, y1, "canvas-rect");
 
   current_path = {
     path: new Path2D(),
@@ -499,7 +670,7 @@ function beginRect(e) {
   path.push(current_path);
   all_path.push(current_path);
 
-  document.querySelector(".canvas-wrapper").appendChild(new_canvas);
+  canvas_wrapper.appendChild(new_canvas);
 
   canvas_wrapper.onmousemove = function(e) {
     x = e.clientX - x1;
@@ -533,7 +704,134 @@ function beginRect(e) {
   };
 }
 
+//рисуем линию
+function draw_line(e) {
+  let x1 = e.clientX;
+  let y1 = e.clientY - widthHeader;
+  let x, y, begin_x, begin_y, end_x, end_y;
+
+  create_canvas(x1, y1, "canvas-line");
+
+  current_path = {
+    path: new Path2D(),
+    isPath: true,
+    context: new_c,
+    stroke: true,
+    fill: false,
+    fillStyle: ["#fff"],
+    shape: "line",
+    target: new_canvas,
+    remove: new_canvas
+  };
+
+  path.push(current_path);
+  all_path.push(current_path);
+
+  canvas_wrapper.appendChild(new_canvas);
+
+  new_c.beginPath();
+
+  canvas_wrapper.onmousemove = function(e) {
+    x = e.clientX - x1;
+    y = e.clientY - y1 - widthHeader;
+
+    if (e.clientX < x1) {
+      new_canvas.style.left = e.clientX + "px";
+      x = x1 - e.clientX;
+    }
+    if (e.clientY - widthHeader < y1) {
+      new_canvas.style.top = e.clientY - widthHeader + "px";
+      y = y1 - (e.clientY - widthHeader);
+    }
+    if (e.clientY - widthHeader > y1 && e.clientX > x1) {
+      begin_x = 0;
+      begin_y = 0;
+      end_x = x;
+      end_y = y;
+    }
+    if (e.clientY - widthHeader < y1 && e.clientX > x1) {
+      begin_x = 0;
+      begin_y = y;
+      end_x = x;
+      end_y = 0;
+    }
+    if (e.clientY - widthHeader > y1 && e.clientX < x1) {
+      begin_x = x;
+      begin_y = 0;
+      end_x = 0;
+      end_y = y;
+    }
+    if (e.clientY - widthHeader < y1 && e.clientX < x1) {
+      begin_x = x;
+      begin_y = y;
+      end_x = 0;
+      end_y = 0;
+    }
+
+    new_canvas.width = x || 1;
+    new_canvas.height = y || 1;
+
+    new_c.moveTo(begin_x, begin_y);
+    new_c.lineTo(end_x, end_y);
+
+    new_c.stroke();
+  };
+  canvas_wrapper.onmouseup = function(e) {
+    path[path.length - 1].path.moveTo(begin_x, begin_y);
+    path[path.length - 1].path.lineTo(end_x, end_y);
+
+    canvas_wrapper.onmousemove = null;
+  };
+}
+
+// resize canvasa
+
 document.body.onresize = function() {
+  canvas_wrapper.style.width = document.documentElement.clientWidth + "px";
+  canvas_wrapper.style.height =
+    document.documentElement.clientHeight - widthHeader + "px";
+
   canvas.width = document.documentElement.clientWidth;
   canvas.height = document.documentElement.clientHeight - widthHeader;
 };
+
+function centering_element(elem) {
+  elem.style.left =
+    document.documentElement.clientWidth / 2 - elem.clientWidth / 2 + "px";
+  elem.style.top =
+    document.documentElement.clientHeight / 2 - elem.clientHeight / 2 + "px";
+}
+
+document
+  .querySelector(".close-wrapper")
+  .addEventListener("mousedown", function(e) {
+    let target = this.parentElement;
+    let x = e.clientX;
+    let y = e.clientY;
+    let begin_x = parseInt(target.style.left) || 0;
+    let begin_y = parseInt(target.style.top) || 0;
+
+    document.onmousemove = function(e) {
+      let new_x = e.clientX - x;
+      let new_y = e.clientY - y;
+
+      target.style.left = begin_x + new_x + "px";
+      target.style.top = begin_y + new_y + "px";
+    };
+
+    target.onmouseup = function() {
+      document.onmousemove = null;
+    };
+
+    document.onmouseup = function() {
+      document.onmousemove = null;
+    };
+  });
+document.querySelector(".close").addEventListener("click", function() {
+  this.parentElement.parentElement.classList.remove("zoomIn");
+  this.parentElement.parentElement.classList.add("zoomOut");
+
+  setTimeout(() => {
+    this.parentElement.parentElement.style.display = "";
+  }, 500);
+});
